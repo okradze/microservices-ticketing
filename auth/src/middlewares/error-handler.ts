ee236c1b747bde08f4c50dc0ab0b28681eec67b0
Mirian Okradze <okradzemirian@gmail.com>
2020-10-25 13:29:10 +0400

    feat: add custom error classes

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   console.log('Something went wrong', err)
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/request-validation-error'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
 
 const router = express.Router()
 
@@ -14,10 +16,10 @@ router.post('/api/users/signup', [
   ],
   (req: Request, res: Response) => {
     const errors = validationResult(req)
-    if(!errors.isEmpty()) throw new Error('Invalid email or password')
+    if(!errors.isEmpty()) throw new RequestValidationError(errors.array())
 
     console.log('Creating user')
-    throw new Error('Error connecting to database')
+    throw new DatabaseConnectionError()
 
     res.send({})
 })
