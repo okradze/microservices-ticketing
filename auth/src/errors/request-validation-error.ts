7db4a46eab33c2ee0bebf2c1db095b49b6884be4
Mirian Okradze <okradzemirian@gmail.com>
2020-10-25 16:32:36 +0400

    feat: serialize custom error response

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,8 +1,17 @@
 import { ValidationError } from 'express-validator'
 
 export class RequestValidationError extends Error {
+  statusCode = 400
+  
   constructor(public errors: ValidationError[]) {
     super()
     Object.setPrototypeOf(this, RequestValidationError.prototype)
+  }
+
+  serializeErrors() {
+    return this.errors.map(error => ({
+      message: error.msg,
+      field: error.param
+    }))
   }
 }
