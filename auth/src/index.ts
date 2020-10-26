bcad9b9c244c429b4944a30437e8326eae32e04f
Mirian Okradze <okradzemirian@gmail.com>
2020-10-26 15:56:06 +0400

    feat: mongodb setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import 'express-async-errors'
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signupRouter } from './routes/signup'
@@ -22,6 +24,21 @@ app.all('*', () => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!')
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    console.log('Connected to mongodb')
+  } catch (error) {
+    console.error(error)
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!')
+  })
+}
+
+start()
