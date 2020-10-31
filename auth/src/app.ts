0057110154e3cb2e43e79fe6c166de6e972bf81a
Mirian Okradze <okradzemirian@gmail.com>
2020-10-31 17:12:41 +0400

    feat: common code

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -3,12 +3,11 @@ import 'express-async-errors'
 import bodyParser from 'body-parser'
 import cookieSession from 'cookie-session'
 
+import { errorHandler, NotFoundError } from '@okradzemirian/ticketing-common'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signupRouter } from './routes/signup'
 import { signoutRouter } from './routes/signout'
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.set('trust proxy', true)
