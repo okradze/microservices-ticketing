45ef7ede080cc6abbd1beccaa43de1729260f09c
Mirian Okradze <okradzemirian@gmail.com>
2020-11-01 17:21:30 +0400

    feat: create ticket

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest'
 import { app } from '../../app'
 import { signin } from '../../test/utils'
+import { Ticket } from '../../models/ticket'
 
 it('can only be accessed if the user is signed in', async () => {
   const response = await request(app)
@@ -65,12 +66,19 @@ it('returns an error if invalid price is provided', async () => {
 })
 
 it('creates a ticket with valid inputs', async () => {
+  let tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(0)
+
+  const ticket = { title: 'Test', price: 20 }
+
   await request(app)
     .post('/api/tickets')
     .set('Cookie', signin())
-    .send({
-      title: 'Test',
-      price: 20,
-    })
+    .send(ticket)
     .expect(201)
+
+  tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(1)
+  expect(tickets[0].title).toEqual(ticket.title)
+  expect(tickets[0].price).toEqual(ticket.price)
 })
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
 import { requireAuth, validateRequest } from '@okradzemirian/ticketing-common'
+import { Ticket } from '../models/ticket'
 
 const router = express.Router()
 
@@ -13,7 +14,10 @@ router.post('/api/tickets', requireAuth, [
     .isFloat({ gt: 0 })
     .withMessage('Price must be greater than 0')
 ], validateRequest, async (req: Request, res: Response) => {
-  res.sendStatus(200)
+  const { title, price } = req.body
+  const ticket = Ticket.build({ title, price, userId: req.currentUser!.id })
+  await ticket.save()
+  res.status(201).send(ticket)
 })
 
 export { router as createTicketRouter }
