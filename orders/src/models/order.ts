3274a0f09f0e792ed20ccf81dd760276d31042a9
Mirian Okradze <okradzemirian@gmail.com>
2020-11-16 23:26:34 +0400

    feat: order model

---
+++ orders/src/models/order.ts
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose'
+import { OrderStatus } from '@okradzemirian/ticketing-common'
+
+interface OrderFields {
+  userId: string,
+  status: OrderStatus,
+  expiresAt: Date,
+  ticket: TicketDoc,
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string,
+  status: OrderStatus,
+  expiresAt: Date,
+  ticket: TicketDoc,
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(fields: OrderFields): OrderDoc
+}
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: Object.values(OrderStatus),
+    default: OrderStatus.Created,
+  },
+  expiresAt: {
+    type: Date,
+  },
+  ticket: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Ticket'
+  }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id
+      delete ret._id
+    }
+  }
+})
+
+orderSchema.statics.build = (fields: OrderFields) => {
+  return new Order(fields)
+}
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema)
+
+export { Order }
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
+import mongoose from 'mongoose'
+import { requireAuth, validateRequest } from '@okradzemirian/ticketing-common'
 
 const router = express.Router()
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+  body('ticketId')
+    .not()
+    .isEmpty()
+    .custom((input: string)=> mongoose.Types.ObjectId.isValid(input))
+    .withMessage('TicketId must be provided')
+], async (req: Request, res: Response) => {
   res.send({})
 })
 
