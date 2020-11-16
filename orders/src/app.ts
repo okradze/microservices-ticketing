8a87c6bb3b95cd4f51a9d012a116ee40705e4566
Mirian Okradze <okradzemirian@gmail.com>
2020-11-16 20:50:25 +0400

    feat: orders depl

---
+++ orders/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import 'express-async-errors'
+import bodyParser from 'body-parser'
+import cookieSession from 'cookie-session'
+
+import { errorHandler, currentUser, NotFoundError } from '@okradzemirian/ticketing-common'
+import { indexTicketRouter } from './routes/index'
+import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
+import { updateTicketRouter } from './routes/update'
+
+const app = express()
+app.set('trust proxy', true)
+app.use(bodyParser.json())
+app.use(cookieSession({
+  signed: false,
+  secure: process.env.NODE_ENV !== 'test'
+}))
+app.use(currentUser)
+
+app.use(indexTicketRouter)
+app.use(createTicketRouter)
+app.use(showTicketRouter)
+app.use(updateTicketRouter)
+
+app.all('*', () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
