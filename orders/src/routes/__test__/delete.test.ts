aa2c2c8b59281afe4036492e0b03df39694a892c
Mirian Okradze <okradzemirian@gmail.com>
2020-11-18 16:23:07 +0400

    feat: orders delete test

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,35 @@
+import request from 'supertest'
+import { app } from '../../app'
+import { signin } from '../../test/utils'
+import { Ticket } from '../../models/ticket'
+import { Order, OrderStatus } from '../../models/order'
+
+it('marks an order as canceled', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price:20,
+  })
+  await ticket.save()
+  
+  const user = signin()
+
+  // create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  // cancel an order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204)
+
+  const updatedOrder = await Order.findById(order.id)
+  expect(updatedOrder!.status).toEqual(OrderStatus.Canceled)
+})
+
+it.todo('emits an order canceled event')
