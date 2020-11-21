90227591dd03b98d650700a507df2a250df58aca
Mirian Okradze <okradzemirian@gmail.com>
2020-11-21 14:56:45 +0400

    feat: order created listener setup in tickets service

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,12 @@
+import { Message } from 'node-nats-streaming'
+import { Listener, OrderCreatedEvent, Subjects } from '@okradzemirian/ticketing-common'
+import { queueGroupName } from './queue-group-name'
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated
+  queueGroupName = queueGroupName
+
+  onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    
+  }
+}
