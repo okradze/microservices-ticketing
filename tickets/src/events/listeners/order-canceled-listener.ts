8b0bf7a1cc39a685c45104fed70da0b961ac34ec
Mirian Okradze <okradzemirian@gmail.com>
2020-11-22 12:34:00 +0400

    feat: order canceled listener

---
+++ tickets/src/events/listeners/order-canceled-listener.ts
@@ -0,0 +1,29 @@
+import { Message } from 'node-nats-streaming'
+import { Listener, OrderCanceledEvent, Subjects } from '@okradzemirian/ticketing-common'
+import { queueGroupName } from './queue-group-name'
+import { Ticket } from '../../models/ticket'
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher'
+
+export class OrderCanceledListener extends Listener<OrderCanceledEvent> {
+  readonly subject = Subjects.OrderCanceled
+  queueGroupName = queueGroupName
+
+  async onMessage(data: OrderCanceledEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id)
+    if(!ticket) throw new Error('Ticket not found')
+
+    ticket.set({ orderId: undefined })
+    await ticket.save()
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      orderId: ticket.orderId,
+      userId: ticket.userId,
+      price: ticket.price,
+      title: ticket.title,
+      version: ticket.version,
+    })
+
+    msg.ack()
+  }
+}
