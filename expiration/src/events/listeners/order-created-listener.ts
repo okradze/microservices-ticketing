7a20a321d93d2fd272ef525fd55a4886d8e40706
Mirian Okradze <okradzemirian@gmail.com>
2020-11-22 22:37:51 +0400

    feat: expiration job

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,20 @@
+import { Message } from 'node-nats-streaming'
+import { Listener, OrderCreatedEvent, Subjects } from '@okradzemirian/ticketing-common'
+import { queueGroupName } from './queueGroupName'
+import { expirationQueue } from '../../queues/expiration-queue'
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated
+  queueGroupName = queueGroupName
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime()
+
+    await expirationQueue.add({
+      orderId: data.id,
+    }, {
+      delay,
+    })
+    msg.ack()
+  }
+}
