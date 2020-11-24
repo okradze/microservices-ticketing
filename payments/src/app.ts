b42f3f7c78eb05454f614e687aa8bf3bcee46367
Mirian Okradze <okradzemirian@gmail.com>
2020-11-24 10:04:47 +0400

    feat: payments service setup

---
+++ payments/src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import 'express-async-errors'
+import bodyParser from 'body-parser'
+import cookieSession from 'cookie-session'
+
+import { errorHandler, currentUser, NotFoundError } from '@okradzemirian/ticketing-common'
+
+const app = express()
+app.set('trust proxy', true)
+app.use(bodyParser.json())
+app.use(cookieSession({
+  signed: false,
+  secure: process.env.NODE_ENV !== 'test'
+}))
+app.use(currentUser)
+
+app.all('*', () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
---
+++ payments/src/test/utils.ts
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+
+export const mongoId = () => new mongoose.Types.ObjectId().toHexString()
+
+export const signin = () => {
+  const payload = {
+    id: mongoId(),
+    email: 'test@test.com',
+  }
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!)
+  const session = JSON.stringify({ jwt: token })
+  const base64 = Buffer.from(session).toString('base64')
+
+  return [`express:sess=${base64}`]
+}
